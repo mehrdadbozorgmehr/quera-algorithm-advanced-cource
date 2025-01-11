c9bdc47511248c4c94ea0fbc1a39bd5b76bbbbc6
M.Bozorgmehr <m.bozorgmehr@emofid.com>
2025-01-11 15:51:56 +0330

    feat: change rotate arrays algorithm

--- src/quera/algorithm/season1/Example3.java
+++ src/quera/algorithm/season1/Example3.java
@@ -1,26 +1,37 @@
 package quera.algorithm.season1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Example3 {
 
-    private void changeOrder(int number) {
-        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4));
-//1234 ==> 4123 , 3412
-        int[] cloneNumbers = new int[4];
-        for (int i = 0; i < numbers.size(); i++) {  // o(n)
-            int p = (number + i) % numbers.size();
-            cloneNumbers[p] = (numbers.get(i));
+    private void changeOrder(int k, int... numbers) {
+        //1234 ==> 4123 , 3412
+        int[] cloneNumbers = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {  // o(n)
+            int p = (k + i) % numbers.length;
+            cloneNumbers[p] = numbers[i];
         }
         System.out.println("SHIFTED ARRAY ==>" + Arrays.toString(cloneNumbers));
     }
 
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // Handle cases where k > n
+
+        for (int i = 0; i < k; i++) {
+            int last = nums[n - 1];
+            for (int j = n - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = last;
+        }
+    }
+
     public static void main(String[] args) {
-//O(n^2)
+        //O(n^2)
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
         Example3 example3 = new Example3();
-        example3.changeOrder(1);
+        example3.changeOrder(3, numbers);
 
     }
 
